08b75ed3e177e0e13511684b11f579ba6d2d0a74
MMTLukas <lwanko.mmt-m2014@fh-salzburg.ac.at>
2015-02-04 13:52:09 +0100

    merge conflict

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # NaturalLanguage2PivotQuery
 creation of pivot queries out of sentences in natural language
+
+# Sentences
+Show me all companies which employ programmers near Salzburg!
+Show me all people that I know who own more than a million euros!
+Show me my friends pictures on mountains!
+Show me all pictures of my friends at the Nova Rock last year!
+Show me all pictures of drunk friends!
--- server.js
+++ server.js
@@ -3,17 +3,6 @@ var config = require('./config.json');
 var spawn = require('child_process').spawn;
 var dependencies = [];
 
-// var child = spawn("java", [
-//   "-mx1024m",
-//   "-cp",
-//   "./parsers/stanford/*",
-//   "edu.stanford.nlp.parser.lexparser.LexicalizedParser",
-//   "-outputFormat",
-//   "penn",
-//   "edu/stanford/nlp/models/lexparser/englishFactored.ser.gz",
-//   "./parsers/stanford/input.txt"
-// ]);
-
 console.log("Reading. Please wait.")
 var child = spawn("java", config.call);
 
