49aee1de19c22cc58d63d737586553db92c9b24a
Christian Lehner <lehner.chri@gmail.com>
2015-02-04 16:30:42 +0100

    working location constraints

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var config = require('./config.json');
-
 var spawn = require('child_process').spawn;
 var dependencies = [];
 
 var createPivotQuery = function (dependencies) {
+  var pivotQueries = [];
   for(var i in dependencies){
     var dependencyArray = dependencies[i].slice(0, dependencies[i].length-3).split("\n")
     var queryObject = null;
@@ -14,32 +14,52 @@ var createPivotQuery = function (dependencies) {
     // XCOMP
     // Search for XCOMP and get child as query object
     for (var j in dependencyArray) {
-      var dependecy = dependencyArray[j].split("(")[0];
+      var dependecy = dependencyArray[j].split("(")[0]; // xcomp(Show-1, people-4) --> extracts 
       if (dependecy === "xcomp") {
-        queryObject = dependencyArray[j].split(", ")[1].split("-")[0];
+        queryObject = dependencyArray[j].split(", ")[1].split("-")[0]; // root(ROOT-0, Show-1) --> extracts Show
         foundQueryObject = true;
       }
     }
 
-    // NSUBJ
-    // When there is no XCOMP, search for the nsubj with "Show" as parent
-    if(!foundQueryObject) {
-      for (var j in dependencyArray) {
-        var dependecy = dependencyArray[j].split("(")[0];
-        if (dependecy === "nsubj") {
-          var parent = dependencyArray[j].split("(")[1].split("-")[0];
-          if(parent == "Show"){
-            queryObject = dependencyArray[j].split(", ")[1].split("-")[0];
-            foundQueryObject = true;
+    // PREP
+    // Search for PREP and decide which additional condition we have to use
+    var constraints = [];
+    for (var j in dependencyArray) {
+      var dependecy = dependencyArray[j].split("(")[0];
+      var constraint = null;
+      if (dependecy === "prep") {
+        var constraint = dependencyArray[j].split(", ")[1].split("-")[0];
+
+        switch(constraint) {
+          case "in":
+            constraint = "location";
+            break;
+          case "on":
+            constraint = "location";
+            break;
+          case "at":
+            constraint = "event";
+          default:
+            break;
+        }
+
+        for (var k in dependencyArray) {
+          var dependecy = dependencyArray[k].split("(")[0];
+          var value = null;
+          if (dependecy === "pobj") {
+            value = dependencyArray[k].split(", ")[1].split("-")[0];
+
+            constraints.push(constraint + ": " + value);
           }
         }
       }
     }
-    console.log(queryObject);
-  }
 
-  // PREP
-  return "?" + queryObject;
+    constraints = constraints.join(";");
+
+    pivotQueries.push("?" + queryObject + ": " + constraints);
+    return pivotQueries; //check only the first sentence
+  }
 }
 
 
